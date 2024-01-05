04ca78e3b8b0243d8e6b4168c37f97d585f09b20
ChenLu <chenlulu@drojian.dev>
2024-01-05 09:54:26 +0800

    Amplituda

--- app/src/main/java/com/san/audioeditor/AmplitudaActivity.kt
+++ app/src/main/java/com/san/audioeditor/AmplitudaActivity.kt
@@ -1,110 +1,271 @@
 package com.san.audioeditor
 
+import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
 import android.view.View
-import android.widget.TextView
-import linc.com.amplituda.Amplituda
-import linc.com.amplituda.AmplitudaProgressListener
-import linc.com.amplituda.AmplitudaResult
-import linc.com.amplituda.Cache
-import linc.com.amplituda.Compress
-import linc.com.amplituda.InputAudio
-import linc.com.amplituda.ProgressOperation
-import linc.com.amplituda.exceptions.AmplitudaException
-import java.io.File
-import java.util.Arrays
-import java.util.Locale
+import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.masoudss.lib.SeekBarOnProgressChanged
+import com.masoudss.lib.WaveformSeekBar
+import com.masoudss.lib.utils.Utils
+import com.masoudss.lib.utils.WaveGravity
+import com.san.audioeditor.databinding.ActivityAmplitudaBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.util.Random
 
 
-class AmplitudaActivity : BaseActivity() {
+class AmplitudaActivity : AppCompatActivity() {
 
-    override val layoutId: Int
-        get() = R.layout.activity_amplituda
+    private lateinit var binding: ActivityAmplitudaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 隐藏标题栏
+        supportActionBar?.hide()
+        // 设置全屏
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
 
+        binding = ActivityAmplitudaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val amplituda = Amplituda(this)
-        amplituda.setLogConfig(Log.ERROR, true)
-        amplituda.processAudio(
-            "/data/hw_init/product/media/Pre-loaded/Music/Last_Stop.mp3",
-            Compress.withParams(Compress.AVERAGE, 1),
-            Cache.withParams(Cache.REUSE),
-            object : AmplitudaProgressListener() {
-                override fun onStartProgress() {
-                    super.onStartProgress()
-                    println("Start Progress")
-                }
-
-                override fun onStopProgress() {
-                    super.onStopProgress()
-                    println("Stop Progress")
-                }
-
-                override fun onProgress(operation: ProgressOperation, progress: Int) {
-                    var currentOperation = ""
-                    currentOperation = when (operation) {
-                        ProgressOperation.PROCESSING -> "Process audio"
-                        ProgressOperation.DECODING -> "Decode audio"
-                        ProgressOperation.DOWNLOADING -> "Download audio from url"
-                    }
-                    System.out.printf("%s: %d%% %n", currentOperation, progress)
+        binding.waveformSeekBar.apply {
+            progress = 33.2F
+            waveWidth = Utils.dp(this@AmplitudaActivity, 5)
+            waveGap = Utils.dp(this@AmplitudaActivity, 2)
+            waveMinHeight = Utils.dp(this@AmplitudaActivity, 5)
+            waveCornerRadius = Utils.dp(this@AmplitudaActivity, 2)
+            waveGravity = WaveGravity.CENTER
+            waveBackgroundColor = ContextCompat.getColor(this@AmplitudaActivity, com.masoudss.lib.R.color.yellow)
+            waveProgressColor = ContextCompat.getColor(this@AmplitudaActivity, com.masoudss.lib.R.color.blue)
+            sample = getDummyWaveSample()
+            marker = getDummyMarkerSample()
+            onProgressChanged = object : SeekBarOnProgressChanged {
+                override fun onProgressChanged(
+                    waveformSeekBar: WaveformSeekBar,
+                    progress: Float,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser)
+                        binding.waveProgress.progress = progress.toInt()
                 }
             }
-        )[{ result: AmplitudaResult<String?>? ->
-            printResult(
-                result!!
-            )
-        }, { exception: AmplitudaException -> exception.printStackTrace() }]
-    }
-
-    override fun onViewClick(view: View) {
-    }
-
-    override fun onSelectedFile(filePath: String) {
-    }
-
-    private fun printResult(result: AmplitudaResult<*>) {
-        val resultStr = String.format(
-            Locale.US,
-            """
-             Audio info:
-             millis = %d
-             seconds = %d
-             
-             source = %s
-             source type = %s
-             
-             Amplitudes:
-             size: = %d
-             list: = %s
-             amplitudes for second 1: = %s
-             json: = %s
-             single line sequence = %s
-             new line sequence = %s
-             custom delimiter sequence = %s
-             %n
-             """.trimIndent(),
-            result.getAudioDuration(AmplitudaResult.DurationUnit.MILLIS),
-            result.getAudioDuration(AmplitudaResult.DurationUnit.SECONDS),
-            if (result.inputAudioType == InputAudio.Type.FILE) (result.audioSource as File).absolutePath else result.audioSource,
-            result.inputAudioType.name,
-            result.amplitudesAsList().size,
-            Arrays.toString(result.amplitudesAsList().toTypedArray()),
-            Arrays.toString(result.amplitudesForSecond(1).toTypedArray()),
-            result.amplitudesAsJson(),
-            result.amplitudesAsSequence(AmplitudaResult.SequenceFormat.SINGLE_LINE),
-            result.amplitudesAsSequence(AmplitudaResult.SequenceFormat.NEW_LINE),
-            result.amplitudesAsSequence(AmplitudaResult.SequenceFormat.SINGLE_LINE, " * ")
-        )
-        if (!TextUtils.isEmpty(resultStr)) {
-            findViewById<TextView>(R.id.desc).text = resultStr
         }
-        Log.i("llc_audio", resultStr)
+
+        binding.waveWidth.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveformSeekBar.waveWidth =
+                    progress / 100F * Utils.dp(this@AmplitudaActivity, 20)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.waveCornerRadius.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveformSeekBar.waveCornerRadius =
+                    progress / 100F * Utils.dp(this@AmplitudaActivity, 10)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.waveGap.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveformSeekBar.waveGap =
+                    progress / 100F * Utils.dp(this@AmplitudaActivity, 10)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.waveProgress.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveformSeekBar.progress = progress.toFloat()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.waveMaxProgress.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveProgress.max = progress
+                binding.waveformSeekBar.maxProgress = progress.toFloat()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.visibleProgress.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.waveformSeekBar.visibleProgress = progress.toFloat()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        binding.gravityRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = binding.gravityRadioGroup.findViewById(checkedId) as RadioButton
+            val index = binding.gravityRadioGroup.indexOfChild(radioButton)
+            binding.waveformSeekBar.waveGravity = when (index) {
+                0 -> WaveGravity.TOP
+                1 -> WaveGravity.CENTER
+                else -> WaveGravity.BOTTOM
+            }
+        }
+
+        binding.waveColorRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = binding.waveColorRadioGroup.findViewById(checkedId) as RadioButton
+            val index = binding.waveColorRadioGroup.indexOfChild(radioButton)
+            binding.waveformSeekBar.waveBackgroundColor = when (index) {
+                0 -> ContextCompat.getColor(this, com.masoudss.lib.R.color.pink)
+                1 -> ContextCompat.getColor(this, com.masoudss.lib.R.color.yellow)
+                else -> ContextCompat.getColor(this, com.masoudss.lib.R.color.white)
+            }
+        }
+
+        binding.progressColorRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+
+            val radioButton = binding.progressColorRadioGroup.findViewById(checkedId) as RadioButton
+            val index = binding.progressColorRadioGroup.indexOfChild(radioButton)
+            binding.waveformSeekBar.waveProgressColor = when (index) {
+                0 -> ContextCompat.getColor(this, com.masoudss.lib.R.color.red)
+                1 -> ContextCompat.getColor(this, com.masoudss.lib.R.color.blue)
+                else -> ContextCompat.getColor(this, com.masoudss.lib.R.color.green)
+            }
+        }
+
+        binding.icGithub.setOnClickListener {
+            val url = "https://github.com/massoudss/waveformSeekBar"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+
+        binding.icImport.setOnClickListener {
+            checkStoragePermission()
+        }
     }
 
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null && requestCode == REQ_CODE_PICK_SOUND_FILE && resultCode == Activity.RESULT_OK) {
+            val path = data.getStringExtra("path")
 
+            val progressDialog = ProgressDialog(this@AmplitudaActivity)
+            progressDialog.setMessage(getString(com.masoudss.lib.R.string.message_waiting))
+            progressDialog.show()
+
+
+            doAsync {
+                binding.waveformSeekBar.setSampleFrom(path!!)
+
+                uiThread {
+                    progressDialog.dismiss()
+                }
+            }
+        }
+    }
+
+    private fun checkStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val hasReadPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            val hasWritePermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+            val permissions = ArrayList<String>()
+
+            if (hasReadPermission != PackageManager.PERMISSION_GRANTED)
+                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+            if (hasWritePermission != PackageManager.PERMISSION_GRANTED)
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+            if (permissions.isNotEmpty())
+                requestPermissions(permissions.toTypedArray(), REQ_CODE_STORAGE_PERMISSION)
+            else
+                launchSelectAudioActivity()
+
+        } else
+            launchSelectAudioActivity()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQ_CODE_STORAGE_PERMISSION) {
+            var denied = false
+            for (i in permissions.indices)
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    denied = true
+                    break
+                }
+
+            if (denied)
+                Toast.makeText(
+                    this@AmplitudaActivity,
+                    getString(com.masoudss.lib.R.string.permission_error),
+                    Toast.LENGTH_SHORT
+                ).show()
+            else
+                launchSelectAudioActivity()
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun launchSelectAudioActivity() {
+//        val intent = Intent(this@AmplitudaActivity, SelectAudioActivity::class.java)
+//        startActivityForResult(intent, REQ_CODE_PICK_SOUND_FILE)
+    }
+
+    private fun getDummyWaveSample(): IntArray {
+        val data = IntArray(50)
+        for (i in data.indices)
+            data[i] = Random().nextInt(data.size)
+
+        return data
+    }
+
+    private fun getDummyMarkerSample(): HashMap<Float, String> {
+        val map = hashMapOf<Float, String>()
+        map[binding.waveformSeekBar.maxProgress / 2] = "Middle"
+        map[binding.waveformSeekBar.maxProgress / 4] = "Quarter"
+        return map
+    }
+
+    companion object {
+        const val REQ_CODE_PICK_SOUND_FILE = 1
+        const val REQ_CODE_STORAGE_PERMISSION = 2
+    }
 }
