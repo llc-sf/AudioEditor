d7a29d0f621f6cdf2a08f2746e9999f92d94b142
ChenLu <chenlulu@drojian.dev>
2024-01-05 11:45:13 +0800

    add

--- app/src/main/java/com/san/audioeditor/BaseActivity.kt
+++ app/src/main/java/com/san/audioeditor/BaseActivity.kt
@@ -14,6 +14,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import dev.audio.ffmpeglib.FFmpegApplication
+import dev.audio.ffmpeglib.tool.ContentUtil
 import java.lang.Exception
 
 /**
--- ffmpegLib/src/main/java/dev/audio/ffmpeglib/tool/ContentUtil.kt
+++ ffmpegLib/src/main/java/dev/audio/ffmpeglib/tool/ContentUtil.kt
@@ -1,4 +1,4 @@
-package com.san.audioeditor
+package dev.audio.ffmpeglib.tool
 
 import android.annotation.TargetApi
 import android.content.ContentUris
