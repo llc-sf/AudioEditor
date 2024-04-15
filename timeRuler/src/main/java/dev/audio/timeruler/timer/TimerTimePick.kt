298603a2269f5dbc0dba948f2e37ff22e1c8581d
ChenLu <chenlulu@drojian.dev>
2024-04-15 18:03:45 +0800

    code review

--- timeRuler/src/main/java/dev/audio/timeruler/timer/TimerTimePick.kt
+++ timeRuler/src/main/java/dev/audio/timeruler/timer/TimerTimePick.kt
@@ -93,6 +93,7 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
         if (binding.hour.value == cutTime.maxTime.hours) { //最大值处理
             if (oldSecond > cutTime.maxTime.second) { //秒大于最大  分数最大范围=最大分数-1
                 binding.minute.maxValue = cutTime.maxTime.minutes - 1
+                binding.minute.value = oldMinute
             } else if (oldSecond == cutTime.maxTime.second) {
                 if (cutTime.maxTime.minutes == binding.minute.value && (binding.msecond.value > cutTime.maxTime.secondDecimal)) { //秒等于最大，分数秒大于最大  分数秒归0
                     binding.msecond.value = 0
@@ -102,6 +103,7 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
         if(binding.hour.value == cutTime.minTime.hours){
             if (oldSecond < cutTime.minTime.second){
                 binding.minute.minValue = cutTime.minTime.minutes + 1
+                binding.minute.value = oldMinute
             }else if(oldSecond == cutTime.minTime.second){
                 if (cutTime.minTime.minutes == binding.minute.value && (binding.msecond.value < cutTime.minTime.secondDecimal)) { //秒等于最大，分数秒大于最大  分数秒归0
                     binding.msecond.value = cutTime.minTime.secondDecimal
