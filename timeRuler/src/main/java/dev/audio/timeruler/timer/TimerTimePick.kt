eab27151b2d4f581fcf550582a04fa3b6cda4f80
ChenLu <chenlulu@drojian.dev>
2024-04-15 16:14:38 +0800

    trim range

--- timeRuler/src/main/java/dev/audio/timeruler/timer/TimerTimePick.kt
+++ timeRuler/src/main/java/dev/audio/timeruler/timer/TimerTimePick.kt
@@ -3,6 +3,7 @@ package dev.audio.timeruler.timer
 import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
@@ -30,40 +31,83 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
         orientation = HORIZONTAL
         gravity = Gravity.CENTER
         binding = ViewSleepTimerPickBinding.inflate(LayoutInflater.from(context), this)
-
     }
 
     private fun initView() {
         binding.hour.setDisplayedValues(getHourDisplayValue())
-        binding.hour.minValue = 0
+        binding.hour.minValue = cutTime.minTime.hours
         binding.hour.maxValue = cutTime.maxTime.hours
         binding.hour.setContentTextTypeface(Typeface.DEFAULT_BOLD)
         binding.hour.setOnValueChangedListener(this)
         binding.hour.setOnValueChangeListenerInScrolling(this)
 
         binding.minute.setDisplayedValues(getMinuteDisplayValue())
-        binding.minute.minValue = 0
-        binding.minute.maxValue = 59
+        binding.minute.minValue = if (cutTime.minTime.hours == cutTime.time.hours) cutTime.minTime.minutes else 0
+        binding.minute.maxValue = if (cutTime.maxTime.hours == cutTime.time.hours) cutTime.maxTime.minutes else 59
         binding.minute.setContentTextTypeface(Typeface.DEFAULT_BOLD)
         binding.minute.setOnValueChangedListener(this)
         binding.minute.setOnValueChangeListenerInScrolling(this)
 
         binding.second.setDisplayedValues(getMinuteDisplayValue())
-        binding.second.minValue = 0
-        binding.second.maxValue = 59
+        binding.second.minValue = if (cutTime.minTime.hours == cutTime.time.hours && cutTime.minTime.minutes == cutTime.time.minutes) cutTime.minTime.second else 0
+        binding.second.maxValue = if (cutTime.maxTime.hours == cutTime.time.hours && cutTime.maxTime.minutes == cutTime.time.minutes) cutTime.maxTime.second else 59
         binding.second.setContentTextTypeface(Typeface.DEFAULT_BOLD)
         binding.second.setOnValueChangedListener(this)
         binding.second.setOnValueChangeListenerInScrolling(this)
 
 
         binding.msecond.setDisplayedValues(getMsSecondDisplayValue())
-        binding.msecond.minValue = 0
-        binding.msecond.maxValue = 9
+        binding.msecond.minValue = if (cutTime.minTime.hours == cutTime.time.hours && cutTime.minTime.minutes == cutTime.time.minutes && cutTime.minTime.second == cutTime.time.second) cutTime.minTime.secondDecimal else 0
+        binding.msecond.maxValue = if (cutTime.maxTime.hours == cutTime.time.hours && cutTime.maxTime.minutes == cutTime.time.minutes && cutTime.maxTime.second == cutTime.time.second) cutTime.maxTime.secondDecimal else 9
         binding.msecond.setContentTextTypeface(Typeface.DEFAULT_BOLD)
         binding.msecond.setOnValueChangedListener(this)
         binding.msecond.setOnValueChangeListenerInScrolling(this)
     }
 
+
+    private fun refresh() {
+        var oldHour = binding.hour.value
+        binding.hour.setDisplayedValues(getHourDisplayValue())
+        binding.hour.minValue = cutTime.minTime.hours
+        binding.hour.maxValue = cutTime.maxTime.hours
+        binding.hour.setContentTextTypeface(Typeface.DEFAULT_BOLD)
+        binding.hour.setOnValueChangedListener(this)
+        binding.hour.setOnValueChangeListenerInScrolling(this)
+        binding.hour.value = oldHour
+
+
+        var oldMinute = binding.minute.value
+        binding.minute.setDisplayedValues(getMinuteDisplayValue())
+        binding.minute.minValue = if (cutTime.minTime.hours == binding.hour.value) cutTime.minTime.minutes else 0
+        binding.minute.maxValue = if (cutTime.maxTime.hours == binding.hour.value) cutTime.maxTime.minutes else 59
+        binding.minute.setContentTextTypeface(Typeface.DEFAULT_BOLD)
+        binding.minute.setOnValueChangedListener(this)
+        binding.minute.setOnValueChangeListenerInScrolling(this)
+        binding.minute.value = oldMinute
+
+
+        var oldSecond = binding.second.value
+        binding.second.setDisplayedValues(getMinuteDisplayValue())
+        binding.second.minValue = if (cutTime.minTime.hours == binding.hour.value && cutTime.minTime.minutes == binding.minute.value) cutTime.minTime.second else 0
+        binding.second.maxValue = if (cutTime.maxTime.hours == binding.hour.value && cutTime.maxTime.minutes == binding.minute.value) cutTime.maxTime.second else 59
+        if (oldSecond >= cutTime.maxTime.second) {
+            binding.minute.maxValue = cutTime.maxTime.minutes - 1
+        }
+        binding.second.setContentTextTypeface(Typeface.DEFAULT_BOLD)
+        binding.second.setOnValueChangedListener(this)
+        binding.second.setOnValueChangeListenerInScrolling(this)
+        binding.second.value = oldSecond
+
+
+        var oldMsecond = binding.msecond.value
+        binding.msecond.setDisplayedValues(getMsSecondDisplayValue())
+        binding.msecond.minValue = if (cutTime.minTime.hours == binding.hour.value && cutTime.minTime.minutes == binding.minute.value && cutTime.minTime.second == binding.second.value) cutTime.minTime.secondDecimal else 0
+        binding.msecond.maxValue = if (cutTime.maxTime.hours == binding.hour.value && cutTime.maxTime.minutes == binding.minute.value && cutTime.maxTime.second == binding.second.value) cutTime.maxTime.secondDecimal else 9
+        binding.msecond.setContentTextTypeface(Typeface.DEFAULT_BOLD)
+        binding.msecond.setOnValueChangedListener(this)
+        binding.msecond.setOnValueChangeListenerInScrolling(this)
+        binding.msecond.value = oldMsecond
+    }
 
     private fun getHourDisplayValue(): Array<String> {
         return mutableListOf<String>().apply {
@@ -91,6 +135,12 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
 
 
     override fun onValueChange(picker: NumberPickerView?, oldVal: Int, newVal: Int) {
+        Log.i("llc_onscroll", "onValueChange: ${picker?.id} $oldVal $newVal") //        when(picker){
+        //            binding.hour->{
+        //
+        //            }
+        //        }
+        refresh()
         mListener?.onSelection(getTime())
         performHapticFeedback(picker)
     }
@@ -102,6 +152,10 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
         return (binding.hour.value * 3600L + binding.minute.value * 60L + binding.second.value) * 1000
     }
 
+    fun getTime(hour: Int, minute: Int, second: Int, msecond: Int): Long {
+        return (hour * 3600L + minute * 60L + second) * 1000 + msecond * 100
+    }
+
     fun setTimeSelectionListener(listener: OnTimeSelectionListener?) {
         this.mListener = listener
     }
@@ -109,26 +163,10 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
     private lateinit var cutTime: DialogTimerSetting.CutTime
 
 
-    /**
-     * 设置固定的时间
-     * @param time 毫秒数
-     */
-    fun setTime(time: Long) {
-
-        val _hour = (time / 1000) / 3600
-        val _minute = ((time / 1000) % 3600) / 60
-        val _second = (time / 1000) % 60
-        val _msecond = (time % 1000) / 100
-        binding.hour.value = _hour.toInt()
-        binding.minute.value = _minute.toInt()
-        binding.second.value = _second.toInt()
-        binding.msecond.value = _msecond.toInt()
-    }
-
     fun setTime(cutTime: DialogTimerSetting.CutTime) {
+        Log.i("llc_onscroll", "setTime: ")
         this.cutTime = cutTime
         initView()
-
         binding.hour.value = this.cutTime.time.hours
         binding.minute.value = this.cutTime.time.minutes
         binding.second.value = this.cutTime.time.second
@@ -144,6 +182,8 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
     }
 
     override fun onValueChangeInScrolling(picker: NumberPickerView?, oldVal: Int, newVal: Int) {
+        Log.i("llc_onscroll", "onValueChangeInScrolling: oldVal: $oldVal, newVal: $newVal")
+
         performHapticFeedback(picker)
     }
 
@@ -157,4 +197,6 @@ class TimerTimePick @JvmOverloads constructor(context: Context, attrs: Attribute
             e.printStackTrace()
         }
     }
+
+
 }
