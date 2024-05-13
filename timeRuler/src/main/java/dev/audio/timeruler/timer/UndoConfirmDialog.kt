685194097efb8c3017e446c88588e31023e9184c
ChenLu <chenlulu@drojian.dev>
2024-05-13 16:00:29 +0800

    ui

--- timeRuler/src/main/java/dev/audio/timeruler/timer/UndoConfirmDialog.kt
+++ timeRuler/src/main/java/dev/audio/timeruler/timer/UndoConfirmDialog.kt
@@ -1,10 +1,15 @@
 package dev.audio.timeruler.timer
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import dev.audio.timeruler.R
 import dev.audio.timeruler.databinding.EditExitDialogBinding
 import dev.audio.timeruler.databinding.UndoConfirmDialogBinding
 
@@ -53,6 +58,22 @@ class UndoConfirmDialog : BaseBottomTranslucentDialog() {
             dismiss()
             onConfirmListener?.onCancel()
         }
+
+        try { // 假设你已经有一个 TextView 实例叫 textView
+            val text = context?.resources?.getString(R.string.undo_confrim)
+                ?: "" // 创建一个 SpannableString
+            var keyText = "‘Confirm’"
+            val spannable = SpannableString(text) // 查找 "Confirm" 单词在字符串中的位置
+            val start = text.indexOf(keyText)
+            val end = start + keyText.length // 创建一个颜色 span 来改变文本颜色
+            val colorSpan = ForegroundColorSpan(context!!.resources!!.getColor(R.color.colorAccent)) // 应用这个 span
+            spannable.setSpan(colorSpan, start, end, Spanned.SPAN_INCLUSIVE_INCLUSIVE) // 设置 TextView 的文本
+            binding.tvTitle.text = spannable
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+
     }
 
     override fun onDestroyView() {
