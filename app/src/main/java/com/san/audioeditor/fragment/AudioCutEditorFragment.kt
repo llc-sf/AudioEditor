bffd8551bfecb8c29b6c762df3e4fb63ec8d5e72
ChenLu <chenlulu@drojian.dev>
2024-05-23 16:34:04 +0800

    ui

--- app/src/main/java/com/san/audioeditor/fragment/AudioCutEditorFragment.kt
+++ app/src/main/java/com/san/audioeditor/fragment/AudioCutEditorFragment.kt
@@ -205,7 +205,7 @@ class AudioCutEditorFragment : BaseMVVMFragment<FragmentAudioCutBinding>(),
             tipsView.measure(widthMeasureSpec, heightMeasureSpec)
             (rootView as? FrameLayout)?.addView(tipsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT))
             tipsView.updateLayoutParams<FrameLayout.LayoutParams> {
-                var margin = 20.dp
+                var margin = 0.dp
                 topMargin = location[1] - tipsView.measuredHeight - margin
                 marginStart = 20.dp
             }
@@ -244,7 +244,7 @@ class AudioCutEditorFragment : BaseMVVMFragment<FragmentAudioCutBinding>(),
             tipsView.measure(widthMeasureSpec, heightMeasureSpec)
             (rootView as? FrameLayout)?.addView(tipsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT))
             tipsView.updateLayoutParams<FrameLayout.LayoutParams> {
-                var margin = 20.dp
+                var margin = 0.dp
                 topMargin = location[1] + ancherView.measuredHeight + margin
                 marginStart = ScreenUtil.getScreenWidth(requireContext()) - tipsView.measuredWidth - margin
             }
@@ -299,7 +299,7 @@ class AudioCutEditorFragment : BaseMVVMFragment<FragmentAudioCutBinding>(),
             tipsView.measure(widthMeasureSpec, heightMeasureSpec)
             (rootView as? FrameLayout)?.addView(tipsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT))
             tipsView.updateLayoutParams<FrameLayout.LayoutParams> {
-                var margin = 20.dp
+                var margin = 0.dp
                 topMargin = location[1] + ancherView.measuredHeight + margin
                 marginStart = location[0]
             }
@@ -361,7 +361,7 @@ class AudioCutEditorFragment : BaseMVVMFragment<FragmentAudioCutBinding>(),
             tipsView.measure(widthMeasureSpec, heightMeasureSpec)
             (rootView as? FrameLayout)?.addView(tipsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT))
             tipsView.updateLayoutParams<FrameLayout.LayoutParams> {
-                var margin = 20.dp
+                var margin = 0.dp
                 topMargin = location[1] - tipsView.measuredHeight - margin
                 marginStart = location[0]
             }
@@ -403,7 +403,7 @@ class AudioCutEditorFragment : BaseMVVMFragment<FragmentAudioCutBinding>(),
             tipsView.measure(widthMeasureSpec, heightMeasureSpec)
             (rootView as? FrameLayout)?.addView(tipsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT))
             tipsView.updateLayoutParams<FrameLayout.LayoutParams> {
-                var margin = 20.dp
+                var margin = 0.dp
                 topMargin = location[1] - tipsView.measuredHeight - margin
                 marginStart = ScreenUtil.getScreenWidth(requireContext()) / 2 - tipsView.measuredWidth / 2
             }
