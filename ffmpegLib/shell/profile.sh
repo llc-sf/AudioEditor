b90fe35a5d6e7d8ce1875bac700757dd0de82d6d
ChenLu <chenlulu@drojian.dev>
2024-06-06 11:48:57 +0800

    add

---
+++ ffmpegLib/shell/profile.sh
@@ -0,0 +1,20 @@
+
+export ANDROID_HOME=/Users/chenlu/Library/Android/sdk
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+export GOOGLE_CLOUD_SDK_HOME=/Users/chenlu/work/google-cloud-sdk/bin
+export PATH=${PATH}:${GOOGLE_CLOUD_SDK_HOME}/
+JAVA_HOME=/Users/chenlu/Library/Java/JavaVirtualMachines/azul-11.0.15/Contents/Home
+PATH=$JAVA_HOME/bin:$PATH:.
+CLASSPATH=$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:.
+export JAVA_HOME
+export PATH
+export CLASSPATH
+export flutter_home=/Users/chenlu/work/flutter/bin
+export PATH=${PATH}:${flutter_home}
+export PATH=$PATH:$HOME/.maestro/bin
+export NDK_PATH=/Users/chenlu/Library/Android/sdk/ndk/android-ndk-r20b
+export NDK=/Users/chenlu/Library/Android/sdk/ndk/android-ndk-r20b
+export TOOL=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/bin
+export SYSROOT=$NDK/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
