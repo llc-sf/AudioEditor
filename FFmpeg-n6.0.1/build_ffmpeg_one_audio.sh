da822e4cfb56c9d89ce1b083c936baf651f91ae5
ChenLu <chenlulu@drojian.dev>
2024-06-06 16:31:50 +0800

    add

--- FFmpeg-n6.0.1/build_ffmpeg_one_audio.sh
+++ FFmpeg-n6.0.1/build_ffmpeg_one_audio.sh
@@ -109,7 +109,6 @@ eac3,flac,g722,g726,g729,m4a,mp3float,mp3,mp3adufloat,mp3adu,mp3on4,opus,pcm_ala
 pcm_mulaw,pcm_dvd,pcm_f16le,pcm_f24le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_s16be,pcm_s16le,\
 pcm_s24be,pcm_s24le,pcm_s32be,pcm_s32le,pcm_s64be,pcm_s64le,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,\
 pcm_u32be,pcm_u32le,pcm_vidc,pcm_zork,truehd,truespeech,vorbis,wmav1,wmav2,\
-ssa,ass,dvbsub,dvdsub,pgssub,mov_text,sami,srt,subrip,text,webvtt \
   --enable-jni \
   --enable-mediacodec \
   --enable-muxers \
