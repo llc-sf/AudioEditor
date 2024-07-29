38c6b78384ac6ed6f61d10bc22a992e74b89f2e9
ChenLu <chenlulu@drojian.dev>
2024-07-29 10:02:34 +0800

    支持 mp2

--- FFmpeg-n6.0.1/build_ffmpeg_one_audio.sh
+++ FFmpeg-n6.0.1/build_ffmpeg_one_audio.sh
@@ -114,7 +114,7 @@ aac,aac_latm,ac3,adpcm_ima_qt,adpcm_ima_wav,adpcm_ms,alac,amrnb,amrwb,ape,dolby_
 eac3,flac,g722,g726,g729,m4a,mp3float,mp3,mp3adufloat,mp3adu,mp3on4,opus,pcm_alaw,\
 pcm_mulaw,pcm_dvd,pcm_f16le,pcm_f24le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,pcm_s16be,pcm_s16le,\
 pcm_s24be,pcm_s24le,pcm_s32be,pcm_s32le,pcm_s64be,pcm_s64le,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,\
-pcm_u32be,pcm_u32le,pcm_vidc,pcm_zork,truehd,truespeech,vorbis,wmav1,wmav2,\
+pcm_u32be,pcm_u32le,pcm_vidc,pcm_zork,truehd,truespeech,vorbis,wmav1,wmav2,mp2\
   --enable-jni \
   --enable-mediacodec \
   --enable-muxers \
@@ -125,7 +125,7 @@ pcm_u32be,pcm_u32le,pcm_vidc,pcm_zork,truehd,truespeech,vorbis,wmav1,wmav2,\
   --enable-demuxer=aac,ac3,alaw,amr,amrnb,amrwb,ape,asf,asf_o,ass,codec2,concat,dash,eac3,flac,\
 g722,g726,g729,gif,gif_pipe,hls,image2,image2pipe,jpeg_pipe,lrc,matroska,webm,mjpeg,mov,m4a,3gp,mp3,\
 mpegts,mv,mulaw,manifest,ogg,pcm_s16be,pcm_s16le,pcm_s32be,pcm_s32le,pcm_f32be,pcm_f32le,pcm_f64be,pcm_f64le,\
-png_pipe,rm,rtp,rtsp,wav,webm_dash,xmv \
+png_pipe,rm,rtp,rtsp,wav,webm_dash,xmv,mp2 \
 $ADDITIONAL_CONFIGURE_FLAG
 make -j
 make install
